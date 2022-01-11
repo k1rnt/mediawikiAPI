58482e5e4017176115d732adb37836530adf4b82
k1rnt <eru08tera15mora@gmail.com>
2022-01-11 18:23:18 +0900

    fix: issue into query

--- pkg/api/login.go
+++ pkg/api/login.go
@@ -1,7 +1,6 @@
 package api
 
 import (
-	"encoding/json"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -10,47 +9,6 @@ import (
 
 	"github.com/k1rnt/mediawikiAPI/config"
 )
-
-type LoginToken struct {
-	Batchcomplete string `json:"batchcomplete"`
-	Query         struct {
-		Tokens struct {
-			Logintoken string `json:"logintoken"`
-		} `json:"tokens"`
-	} `json:"query"`
-}
-
-func GetLoginToken(cli *http.Client) LoginToken {
-	request, err := http.NewRequest(http.MethodGet, config.Endpoint, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	params := request.URL.Query()
-	params.Add("action", "query")
-	params.Add("meta", "tokens")
-	params.Add("type", "login")
-	params.Add("format", "json")
-	request.URL.RawQuery = params.Encode()
-
-	response, err := cli.Do(request)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer response.Body.Close()
-
-	body, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var logintoken LoginToken
-	if err := json.Unmarshal(body, &logintoken); err != nil {
-		log.Fatal(err)
-	}
-
-	return logintoken
-}
 
 func GetLoginRequest(cli *http.Client, token string) []byte {
 	data := url.Values{}
