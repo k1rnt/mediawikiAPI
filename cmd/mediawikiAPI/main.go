b89ade2b77a6b868a685a7d19ce40ca512e01aaa
k1rnt <eru08tera15mora@gmail.com>
2022-01-11 18:24:23 +0900

    add: main call example

--- cmd/mediawikiAPI/main.go
+++ cmd/mediawikiAPI/main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/cookiejar"
+	"time"
 
+	"github.com/k1rnt/mediawikiAPI/pkg/api"
+)
+
+func setClient() *http.Client {
+	jar, err := cookiejar.New(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	timeout := time.Duration(5 * time.Second)
+	client := &http.Client{
+		Jar:     jar,
+		Timeout: timeout,
+	}
+	return client
+}
+
+func main() {
+	cli := setClient()
+	logintoken := api.GetLoginToken(cli).Query.Tokens.Logintoken
+	api.GetLoginRequest(cli, logintoken)
+	fmt.Printf("%s\n", api.CsrfToken(cli).Query.Tokens.Csrftoken)
 }
