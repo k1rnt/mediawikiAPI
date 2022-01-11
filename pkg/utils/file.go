b2f81b5280c1e20d6bd83b3c41767a9e20de5689
k1rnt <eru08tera15mora@gmail.com>
2022-01-11 20:09:35 +0900

    add: Function to file exists check

--- pkg/utils/file.go
+++ pkg/utils/file.go
@@ -2,17 +2,25 @@ package utils
 
 import (
 	"io/fs"
-	"log"
+	"os"
 	"path/filepath"
+
+	"golang.org/x/xerrors"
 )
 
 // ファイルパスから拡張子を取り除いたファイル名を返す
-func GetFileNameWithoutExt(path string) string {
-	return filepath.Base(path[:len(path)-len(filepath.Ext(path))])
+func GetFileNameWithoutExt(path string) (string, error) {
+	if !Exists(path) {
+		return "", xerrors.Errorf("%s does not exist", path)
+	}
+	return filepath.Base(path[:len(path)-len(filepath.Ext(path))]), nil
 }
 
 // dir から再起的にファイルを探索しstring sliceで返す
-func FindWikiFiles(dir string) []string {
+func FindWikiFiles(dir string) ([]string, error) {
+	if !Exists(dir) {
+		return nil, xerrors.Errorf("%s does not exist", dir)
+	}
 	var files []string
 	err := filepath.Walk(dir, func(path string, info fs.FileInfo, err error) error {
 		if err != nil {
@@ -22,7 +30,14 @@ func FindWikiFiles(dir string) []string {
 		return nil
 	})
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
-	return files
+	return files, nil
+}
+
+func Exists(path string) bool {
+	if _, err := os.Stat(path); err != nil {
+		return false
+	}
+	return true
 }
