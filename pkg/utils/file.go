10da0c58e0de864b91d3e8fccc25fa9e60cb3d71
k1rnt <eru08tera15mora@gmail.com>
2022-01-11 19:51:34 +0900

    add: Function to output a path from a directory

--- pkg/utils/file.go
+++ pkg/utils/file.go
@@ -1,8 +1,28 @@
 package utils
 
-import "path/filepath"
+import (
+	"io/fs"
+	"log"
+	"path/filepath"
+)
 
 // ファイルパスから拡張子を取り除いたファイル名を返す
 func GetFileNameWithoutExt(path string) string {
 	return filepath.Base(path[:len(path)-len(filepath.Ext(path))])
+}
+
+// dir から再起的にファイルを探索しstring sliceで返す
+func FindWikiFiles(dir string) []string {
+	var files []string
+	err := filepath.Walk(dir, func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		files = append(files, path)
+		return nil
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	return files
 }
