320a92e1eae6c391e5f88743ce5b3a35fbfd3032
k1rnt <eru08tera15mora@gmail.com>
2022-01-12 12:37:11 +0900

    add: file utils functions

--- pkg/utils/file.go
+++ pkg/utils/file.go
@@ -41,6 +41,31 @@ func FindWikiFiles(dir string) ([]string, error) {
 	return files, nil
 }
 
+// dir から再起的に画像ファイルを探索しstring sliceで返す
+func FindImageFiles(dir string) ([]string, error) {
+	if !Exists(dir) {
+		return nil, xerrors.Errorf("%s does not exist", dir)
+	}
+	var files []string
+	err := filepath.Walk(dir, func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		// 画像ファイル以外は飛ばす
+		if !isImageFile(path) {
+			return nil
+		}
+
+		files = append(files, path)
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return files, nil
+}
+
 // pathが存在するかどうかを真偽値で返す
 func Exists(path string) bool {
 	if _, err := os.Stat(path); err != nil {
@@ -52,4 +77,15 @@ func Exists(path string) bool {
 // pathの拡張子がmediawikiかどうかを真偽値で返す
 func isMediawikiFile(path string) bool {
 	return filepath.Ext(path) == ".mediawiki"
+}
+
+// pathの拡張子が画像かどうかを真偽値で返す
+func isImageFile(path string) bool {
+	allowExt := [...]string{".png", ".jpg", ".jpeg", ".gif"}
+	for _, ext := range allowExt {
+		if ext == filepath.Ext(path) {
+			return true
+		}
+	}
+	return false
 }
