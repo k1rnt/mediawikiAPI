8066468288a376c37f463acb736e63cad6980a83
k1rnt <eru08tera15mora@gmail.com>
2022-01-13 17:54:08 +0900

    #2 example

--- cmd/mediawikiAPI/main.go
+++ cmd/mediawikiAPI/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"net/http/cookiejar"
-	"time"
 
 	"github.com/k1rnt/mediawikiAPI/pkg/api"
 	"github.com/k1rnt/mediawikiAPI/pkg/utils"
@@ -15,26 +15,43 @@ func setClient() *http.Client {
 	if err != nil {
 		log.Fatal(err)
 	}
-	timeout := time.Duration(5 * time.Second)
+	// timeout := time.Duration(5 * time.Second)
 	client := &http.Client{
-		Jar:     jar,
-		Timeout: timeout,
+		Jar: jar,
+		// Timeout: timeout,
 	}
 	return client
 }
 
 func main() {
-	// example#1
 	cli := setClient()
 	logintoken := api.GetLoginToken(cli).Query.Tokens.Logintoken
 	api.GetLoginRequest(cli, logintoken)
 	csrftoken := api.CsrfToken(cli).Query.Tokens.Csrftoken
 
-	files, err := utils.FindWikiFiles(dir)
+	// example#1
+	// files, err := utils.FindWikiFiles(wiki_path)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// for _, path := range files {
+	// 	if _, err := api.EditRequest(cli, csrftoken, path); err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// 	fmt.Printf("success %s\n", path)
+	// }
+
+	// example#2
+	images, err := utils.FindImageFiles(images_path)
 	if err != nil {
 		log.Fatal(err)
 	}
-	for _, path := range files {
-		api.EditRequest(cli, csrftoken, path)
+	for _, path := range images {
+		body, err := api.Upload(cli, csrftoken, path)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("%s\n", body)
+		fmt.Printf("success %s\n", path)
 	}
 }
